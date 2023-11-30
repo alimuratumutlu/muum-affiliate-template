5038648c224891c331d46ed465a7a18619d4a0ba
Murat Umutlu <murat@muum.dev>
2023-11-30 20:19:35 +0300

    react-query added for server state

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,21 +1,42 @@
 import "@/styles/globals.css";
 import "@mantine/core/styles.css";
 
-import { AppShell, MantineProvider } from "@mantine/core";
+import { useState } from "react";
+import type { AppProps } from "next/app";
+
+// Server State is the data that is managed by the server and is typically dynamic. It can be things like user data, product listings, etc. Server State is often fetched and used by client-side applications to display up-to-date information.
+// https://react-query.tanstack.com/guides/ssr#hydration
+
+// We are using React Query for this project so we need to wrap our app with the QueryClientProvider
+// https://react-query.tanstack.com/overview#queryclientprovider
+
+import {
+	HydrationBoundary,
+	QueryClient,
+	QueryClientProvider,
+} from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 // We are using Mantine UI for this project so we need to wrap our app with the MantineProvider
 // https://mantine.dev/core/getting-started/#mantineprovider
 
-import type { AppProps } from "next/app";
+import { AppShell, MantineProvider } from "@mantine/core";
 
 import { theme } from "@/styles/theme";
 
 export default function App({ Component, pageProps }: AppProps) {
+	const [queryClient] = useState(() => new QueryClient());
+
 	return (
-		<MantineProvider theme={theme} defaultColorScheme="dark">
-			<AppShell>
-				<Component {...pageProps} />
-			</AppShell>
-		</MantineProvider>
+		<QueryClientProvider client={queryClient}>
+			<HydrationBoundary state={pageProps.dehydratedState}>
+				<MantineProvider theme={theme} defaultColorScheme="dark">
+					<AppShell>
+						<Component {...pageProps} />
+					</AppShell>
+				</MantineProvider>
+			</HydrationBoundary>
+			<ReactQueryDevtools />
+		</QueryClientProvider>
 	);
 }
