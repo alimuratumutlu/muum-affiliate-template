e7fb405b3eaa32317aa4c8ffabba8a989de901ec
Murat Umutlu <murat@muum.dev>
2023-11-30 19:57:31 +0300

    data fetching implementation

---
+++ utils/dataFetcher.ts
@@ -0,0 +1,12 @@
+export const fetchData = async <T>(url: string): Promise<T | null> => {
+	try {
+		const response = await fetch(url);
+		if (!response.ok) {
+			throw new Error(`HTTP error! status: ${response.status}`);
+		}
+		return (await response.json()) as T;
+	} catch (error) {
+		console.error("Fetching error: ", error);
+		return null;
+	}
+};
