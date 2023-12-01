03ec1014a98d566f849c5b0778edf2ebe1acd976
Murat Umutlu <murat@muum.dev>
2023-12-02 02:46:15 +0300

    cart component refactored and added item check

--- components/atoms/Buttons/CartButton.component.tsx
+++ components/atoms/Buttons/CartButton.component.tsx
@@ -1,29 +1,49 @@
-import React from "react";
-
+import React, { useMemo } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { Button } from "@mantine/core";
-import { IconShoppingCartCheck } from "@tabler/icons-react";
+import {
+	IconShoppingCartCheck,
+	IconShoppingCartPlus,
+} from "@tabler/icons-react";
+import { addToCart, selectCart } from "@/store/cart/cartSlice";
 
-import { addToCart } from "@/store/cart/cartSlice";
+import classes from "@/styles/Buttons.module.css";
 
 interface CartButtonProps {
 	id: string;
 }
 
 export default function CartButton({ id }: CartButtonProps) {
+	const dispatch = useDispatch();
+	const cart = useSelector(selectCart);
+	const isItemInCart = cart.items[id] !== undefined;
+
+	// We use useMemo to avoid re-rendering the button when the item is already in the cart.
+	// This is because the button's gradient color is dependent on whether the item is in the cart or not.
+
+	const [buttonStartColor, buttonEndColor] = useMemo(() => {
+		return isItemInCart ? ["green", "gray"] : ["blue", "pink"];
+	}, [isItemInCart]);
 	const handleAddToCart = (id: string) => {
-		addToCart(id);
+		dispatch(addToCart(id));
 	};
 
 	return (
 		<Button
 			radius="md"
-			style={{ flex: 1 }}
+			className={classes.fullSizeButton}
 			variant="gradient"
-			gradient={{ from: "blue", to: "pink", deg: 236 }}
-			rightSection={<IconShoppingCartCheck size="0.9rem" />}
+			gradient={{ from: buttonStartColor, to: buttonEndColor, deg: 236 }}
+			rightSection={
+				isItemInCart ? (
+					<IconShoppingCartCheck size="1.2rem" />
+				) : (
+					<IconShoppingCartPlus size="1.2rem" />
+				)
+			}
 			onClick={() => handleAddToCart(id)}
 		>
-			Add to Cart
+			{isItemInCart ? "In Cart" : "Add to Cart"}
 		</Button>
 	);
 }
