6d532b685e321ed437eb7b4c4d81dbb6132be386
Murat Umutlu <murat@muum.dev>
2023-12-01 16:24:04 +0300

    link added for logo

--- components/Logo/Logo.component.tsx
+++ components/Logo/Logo.component.tsx
@@ -1,9 +1,12 @@
 import Image from "next/image";
+import Link from "next/link";
 
 /* Logo will be dependent on theme */
 
 export default function Logo() {
 	return (
-		<Image src={"/muum-dark-mode.png"} alt="logo" width={133} height={55} />
+		<Link href={"/"}>
+			<Image src={"/muum-dark-mode.png"} alt="logo" width={133} height={55} />
+		</Link>
 	);
 }
