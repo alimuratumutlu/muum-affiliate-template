e5ac8867440a2d7747b91ab855d03a7cf1dab6f2
Murat Umutlu <murat@muum.dev>
2023-12-03 13:39:04 +0300

    routes updated

--- routes/HeaderRoutes.ts
+++ routes/HeaderRoutes.ts
@@ -1,6 +1,4 @@
 export const HeaderRoutes = [
-	{ link: "/", label: "Home" },
 	{ link: "/productList", label: "Fashion Store" },
-	{ link: "/about-us", label: "About Us" },
-	{ link: "/contact-us", label: "Contact Us" },
+	{ link: "/statistic", label: "Statistics" },
 ];
